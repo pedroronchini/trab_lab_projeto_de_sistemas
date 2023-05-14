1e5b6405354c5e8e6c1fe37fc90aa20b2c7bbe46
Pedro Ronchini <pe-ronchini@hotmail.com>
2023-05-14 19:31:44 -0300

    create task tests

---
+++ __tests__/taskUpdate.test.ts
@@ -0,0 +1,20 @@
+import request from 'supertest';
+import app from '../src/app';
+
+describe('Task update', () => {
+  it('should update a task', async () => {
+    const updateTask = {
+      completed: 'false',
+      text: 'restore db',
+      cardId: '1'
+    };
+
+    const response = await request(app)
+      .put('/task/1')
+      .send(updateTask)
+      .expect(200);
+
+    expect(response.body).toHaveProperty('id');
+    expect(response.body).toBe(updateTask);
+  });
+});
